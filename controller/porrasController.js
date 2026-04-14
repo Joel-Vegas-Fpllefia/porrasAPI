9ffe9241cebe7c77711cc0e143bc5909c110d0a7
daw2-vegas26 <vegasromerojoel@fpllefia.com>
2026-04-14 20:27:01 +0200

    porras datos

--- controller/porrasController.js
+++ controller/porrasController.js
@@ -3,6 +3,22 @@ const datos = [
     email: "carebola@fpllefia.com",
     porra: "1-5",
   },
+  {
+    email: "ivan@fpllefia.com",
+    porra: "1-0",
+  },
+  {
+    email: "joel@fpllefia.com",
+    porra: "1-3",
+  },
+  {
+    email: "edgar@fpllefia.com",
+    porra: "3-2",
+  },
+  {
+    email: "miguel@fpllefia.com",
+    porra: "1-4",
+  },
 ];
 
 function getPorras(req, res) {
